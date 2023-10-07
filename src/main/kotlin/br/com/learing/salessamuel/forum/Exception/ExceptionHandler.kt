cacafcbbf459559e318b4ac5c351bc4a958ccc8e
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-07 11:40:33 -0300

    feat: add error tratament for BAD_REQUEST

--- src/main/kotlin/br/com/learing/salessamuel/forum/Exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/learing/salessamuel/forum/Exception/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package br.com.learing.salessamuel.forum.Exception
 import br.com.learing.salessamuel.forum.DTO.Views.ErrorView
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -24,10 +25,26 @@ class ExceptionHandler {
         )
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleException(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val messageError = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach { e -> messageError.put(e.field, e.defaultMessage) }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = messageError.toString(),
+            path = request.servletPath
+        )
+    }
+
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleException(
-        exception: java.lang.Exception,
+        exception: Exception,
         request: HttpServletRequest
     ): ErrorView {
         return ErrorView(
